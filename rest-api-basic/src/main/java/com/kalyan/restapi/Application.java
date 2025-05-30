ce34acfe2171d1007109c8e131bcc7d2c71d96a6
Kalyan Dass <kalyan.dass3@gmail.com>
2025-05-30 15:23:06 -0500

    Rest basic template setup

---
+++ rest-api-basic/src/main/java/com/kalyan/restapi/Application.java
@@ -0,0 +1,12 @@
+package com.kalyan.restapi;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+class Application {
+    public static void main(String[] args) {
+        run(Application.class, args);
+    }
+}
